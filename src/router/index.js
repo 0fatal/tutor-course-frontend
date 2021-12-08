0b3d742b463d50093920158518cc36896869292c
ZacharyWin <2816813070@qq.com>
2021-12-08 20:56:11 +0800

    refactor(aside)

--- src/router/index.js
+++ src/router/index.js
@@ -1,161 +1,244 @@
-import en from '../i18n/lang/en'
+// import en from '../i18n/lang/en'
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/views/login/index'
+// import Router from 'vue-router'
+// import Login from '@/views/login/index'
+// import Layout from '@/views/layout/layout'
+// import HomeMain from '@/views/index/mainIndex'
+//
+// const NotFound = () => import('@/views/page404')
+//
+// /**
+//  * 重写路由的push方法
+//  */
+// const routerPush = Router.prototype.push
+// Router.prototype.push = function push (location) {
+//   return routerPush.call(this, location).catch(error => error)
+// }
+// Vue.use(Router)
+// let routeName = en.routeName
+// let defaultRouter = [
+//   {
+//     path: '/',
+//     redirect: '/index',
+//     hidden: true,
+//     children: []
+//   },
+//   {
+//     path: '/login',
+//     component: Login,
+//     name: '',
+//     hidden: true,
+//     children: []
+//   },
+//   {
+//     path: '/index',
+//     iconCls: 'fa fa-dashboard', // 图标样式class
+//     name: routeName.home,
+//     component: Layout,
+//     alone: true,
+//     children: [
+//       {
+//         path: '/index',
+//         iconCls: 'fa fa-dashboard', // 图标样式class
+//         name: '主页',
+//         component: HomeMain,
+//         children: []
+//       }
+//     ]
+//   },
+//   {
+//     path: '/404',
+//     component: NotFound,
+//     name: '404',
+//     hidden: true,
+//     children: []
+//   }
+// ]
+//
+// let addRouter = [
+//   // {
+//   //   path: '/',
+//   //   iconCls: 'fa fa-universal-access', // 图标样式class
+//   //   name: routeName.permissions,
+//   //   component: Layout,
+//   //   children: [
+//   //     {
+//   //       path: '/pagePermissions',
+//   //       iconCls: 'fa fa-expeditedssl', // 图标样式class
+//   //       name: routeName.pageControl,
+//   //       component: pagePermissions,
+//   //       children: []
+//   //     },
+//   //     {
+//   //       path: '/btnPermissions',
+//   //       iconCls: 'fa fa-toggle-on', // 图标样式class
+//   //       name: routeName.btnControl,
+//   //       component: btnPermissions,
+//   //       children: []
+//   //     }
+//   //   ]
+//   // },
+//   {
+//     path: '/',
+//     iconCls: 'fa fa-universal-access',
+//     name: routeName.courseManage,
+//     component: Layout,
+//     meta: {role: ['teacher', 'student']},
+//     children: [
+//       {
+//         path: '/courseManage/docxInfo',
+//         iconCls: 'fa fa-toggle-on',
+//         name: routeName.docxInfo,
+//         component: () => import('@/views/courseManage/docxInfo'),
+//         meta: {
+//           role: ['teacher']
+//         },
+//         children: []
+//       },
+//       {
+//         // path: '/courseManage/createDocx',
+//         path: '/courseManage/createDocx',
+//         iconCls: 'fa fa-toggle-on',
+//         name: routeName.createDocx,
+//         component: () => import('@/views/courseManage/createDocx'),
+//         meta: {role: ['teacher']},
+//         children: []
+//       },
+//       {
+//         path: '/courseManage/information',
+//         iconCls: 'fa fa-toggle-on',
+//         name: routeName.courseInfo,
+//         component: () => import('@/views/courseManage/courseInfo'),
+//         meta: {role: ['teacher']},
+//         children: []
+//       },
+//       {
+//         path: '/courseManage/courseApply',
+//         iconCls: 'fa fa-toggle-on',
+//         name: routeName.courseApply,
+//         component: () => import('@/views/courseManage/courseApply'),
+//         meta: {role: ['student']},
+//         children: []
+//       },
+//       {
+//         path: '/courseManage/template/instance',
+//         iconCls: 'fa fa-toggle-on',
+//         name: 'course-template-instance',
+//         component: () => import('@/views/courseManage/templateInstance'),
+//         meta: {role: ['student']},
+//         children: []
+//       }
+//     ]
+//   },
+//   {
+//     path: '/',
+//     iconCls: 'fa fa-universal-access',
+//     name: routeName.infoManage,
+//     component: Layout,
+//     meta: {role: ['teacher', 'student']},
+//     children: [
+//       {
+//         path: '/infoManage/teacherInfo',
+//         iconCls: 'fa fa-toggle-on',
+//         name: routeName.teacherInfo,
+//         component: () => import('@/views/infoManage/teacherInfo'),
+//         meta: {role: ['teacher']},
+//         children: []
+//       },
+//       {
+//         path: '/infoManage/studentInfo',
+//         iconCls: 'fa fa-toggle-on',
+//         name: routeName.studentInfo,
+//         component: () => import('@/views/infoManage/studentInfo'),
+//         meta: {role: ['student']},
+//         children: []
+//       }
+//     ]
+//   },
+//   {
+//     path: '*',
+//     redirect: '/404',
+//     hidden: true,
+//     children: []
+//   }
+//
+// ]
+// export default new Router({
+//   routes: defaultRouter
+// })
+// export {defaultRouter, addRouter}
+import Login from '@/views/login/'
 import Layout from '@/views/layout/layout'
 import HomeMain from '@/views/index/mainIndex'
 
+
 const NotFound = () => import('@/views/page404')
 
-/**
- * 重写路由的push方法
- */
-const routerPush = Router.prototype.push
-Router.prototype.push = function push (location) {
-  return routerPush.call(this, location).catch(error => error)
-}
-Vue.use(Router)
-let routeName = en.routeName
-let defaultRouter = [
+
+const routes = [
   {
     path: '/',
-    redirect: '/index',
-    hidden: true,
-    children: []
+    redirect: '/index'
   },
   {
     path: '/login',
-    component: Login,
-    name: '',
-    hidden: true,
-    children: []
+    name: 'login',
+    component: Login
   },
   {
     path: '/index',
-    iconCls: 'fa fa-dashboard', // 图标样式class
-    name: routeName.home,
+    name: 'layout',
     component: Layout,
-    alone: true,
     children: [
       {
         path: '/index',
-        iconCls: 'fa fa-dashboard', // 图标样式class
         name: '主页',
-        component: HomeMain,
-        children: []
+        component: HomeMain
+      },
+      {
+        path: '/info/teacher',
+        name: 'info-teacher',
+        component: () => import('@/views/infoManage/teacherInfo')
+      },
+      {
+        path: '/course/template',
+        name: 'course-template',
+        component: () => import('@/views/courseManage/docxInfo')
+      },
+      {
+        path: '/course/template/instance/new',
+        name: 'template-instance-new',
+        component: () => import('@/views/courseManage/createDocx')
+      },
+      {
+        path: '/course/template/instance',
+        name: 'course-template-instance',
+        component: () => import('@/views/courseManage/templateInstance')
+      },
+      {
+        path: '/course',
+        name: 'course',
+        component: () => import('@/views/courseManage/courseInfo')
       }
+
     ]
   },
   {
     path: '/404',
     component: NotFound,
-    name: '404',
-    hidden: true,
-    children: []
-  }
-]
-
-let addRouter = [
-  // {
-  //   path: '/',
-  //   iconCls: 'fa fa-universal-access', // 图标样式class
-  //   name: routeName.permissions,
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: '/pagePermissions',
-  //       iconCls: 'fa fa-expeditedssl', // 图标样式class
-  //       name: routeName.pageControl,
-  //       component: pagePermissions,
-  //       children: []
-  //     },
-  //     {
-  //       path: '/btnPermissions',
-  //       iconCls: 'fa fa-toggle-on', // 图标样式class
-  //       name: routeName.btnControl,
-  //       component: btnPermissions,
-  //       children: []
-  //     }
-  //   ]
-  // },
-  {
-    path: '/',
-    iconCls: 'fa fa-universal-access',
-    name: routeName.courseManage,
-    component: Layout,
-    meta: {role: ['teacher', 'student']},
-    children: [
-      {
-        path: '/courseManage/docxInfo',
-        iconCls: 'fa fa-toggle-on',
-        name: routeName.docxInfo,
-        component: () => import('@/views/courseManage/docxInfo'),
-        meta: {
-          role: ['teacher'],
-        },
-        children: []
-      },
-      {
-        // path: '/courseManage/createDocx',
-        path: '/courseManage/createDocx',
-        iconCls: 'fa fa-toggle-on',
-        name: routeName.createDocx,
-        component: () => import('@/views/courseManage/createDocx'),
-        meta: {role: ['teacher']},
-        children: []
-      },
-      {
-        path: '/courseManage/information',
-        iconCls: 'fa fa-toggle-on',
-        name: routeName.courseInfo,
-        component: () => import('@/views/courseManage/courseInfo'),
-        meta: {role: ['teacher']},
-        children: []
-      },
-      {
-        path: '/courseManage/courseApply',
-        iconCls: 'fa fa-toggle-on',
-        name: routeName.courseApply,
-        component: () => import('@/views/courseManage/courseApply'),
-        meta: {role: ['student']},
-        children: []
-      }
-    ]
-  },
-  {
-    path: '/',
-    iconCls: 'fa fa-universal-access',
-    name: routeName.infoManage,
-    component: Layout,
-    meta: {role: ['teacher', 'student']},
-    children: [
-      {
-        path: '/infoManage/teacherInfo',
-        iconCls: 'fa fa-toggle-on',
-        name: routeName.teacherInfo,
-        component: () => import('@/views/infoManage/teacherInfo'),
-        meta: {role: ['teacher']},
-        children: []
-      },
-      {
-        path: '/infoManage/studentInfo',
-        iconCls: 'fa fa-toggle-on',
-        name: routeName.studentInfo,
-        component: () => import('@/views/infoManage/studentInfo'),
-        meta: {role: ['student']},
-        children: []
-      }
-    ]
+    name: '404'
   },
   {
     path: '*',
-    redirect: '/404',
-    hidden: true,
-    children: []
+    redirect: '/404'
   }
-
 ]
-export default new Router({
-  routes: defaultRouter
+
+Vue.use(Router)
+const router = new Router({
+  routes
 })
-export {defaultRouter, addRouter}
+
+
+export default router
--- src/views/layout/aside/aside.vue
+++ src/views/layout/aside/aside.vue
@@ -2,35 +2,40 @@
   <div>
     <el-aside id="asideNav">
       <div class="logo-name">
-        <p v-if="$store.getters.logoShow">XU</p>
-        <p v-else>vue-xuAdmin后台模板</p>
+        <p>教学材料生成系统</p>
       </div>
       <el-menu :default-active="$route.path" class="el-menu-vertical"
-               @select="selectmenu"
+               @select="selectMenu"
                :collapse="$store.getters.isCollapse"
                background-color="#03152A"
                text-color="rgba(255,255,255,.7)"
-               active-text-color="#ffffff"
-               :router="$store.getters.uniquerouter"
-               :unique-opened="$store.getters.uniquerouter"
+               active-text-color="#e6c56c"
+               router
+               :unique-opened="openTab"
                :collapse-transition="true"
       >
-        <template v-for="(item,index) in $store.getters.routers" v-if="!item.hidden">
-          <el-submenu v-if="!item.alone && item.children.length>0" :index="index+''" :key="index">
-            <template slot="title">
-              <i :class="item.iconCls?item.iconCls:[fa,fa-server]"></i>
-              <span slot="title">{{ $t(`routeName.${item.name}`) }}</span>
-            </template>
-
-            <menu-tree :menuData="item.children"></menu-tree>
-
-          </el-submenu>
-          <el-menu-item :index="item.path" v-else :key="item.path">
-            <i :class="item.iconCls?item.iconCls:[fa,fa-file]"/>
-            <span slot="title">{{ $t(`routeName.${item.name}`) }}</span>
+        <el-submenu :index="1">
+          <template slot="title">
+            <span slot="title">课程相关</span>
+          </template>
+          <el-menu-item :index="1" route="/course">
+            课程列表
           </el-menu-item>
-        </template>
-
+          <el-menu-item :index="2" route="/course/template">
+            模板列表
+          </el-menu-item>
+          <el-menu-item :index="3" route="/course/template/instance">
+            实例列表
+          </el-menu-item>
+        </el-submenu>
+        <el-submenu :index="2">
+          <template slot="title">
+            <span slot="title">教师信息</span>
+          </template>
+          <el-menu-item :index="1" route="/info/teacher">
+            教师个人信息
+          </el-menu-item>
+        </el-submenu>
       </el-menu>
     </el-aside>
   </div>
@@ -44,14 +49,19 @@ export default {
   components: {
     menuTree
   },
+  data () {
+    return {
+      openTab: ''
+    }
+  },
   watch: {
     // 监听浏览器直接输入路由，将此路由添加到tabnavBox
     '$route.path': function (val) {
-      this.selectmenu(val)
+      this.selectMenu(val)
     }
   },
   methods: {
-    selectmenu (key, indexpath) {
+    selectMenu (key, indexpath) {
       // 如果不使用 elemenUI 菜单的 vue-router 的模式将用以下方式进行页面跳转 el-menu的router设置为false
       // this.$router.push(indexpath.join("/"))
       let router = this.$store.getters.routers
@@ -78,83 +88,93 @@ export default {
 </script>
 
 <style lang="scss">
-  $top: top;
-  $bottom: bottom;
-  $left: left;
-  $right: right;
-  %w100 {
-    width: 100%;
-  }
+$top: top;
+$bottom: bottom;
+$left: left;
+$right: right;
+%w100 {
+  width: 100%;
+}
 
-  %h100 {
-    height: 100%;
-  }
+%h100 {
+  height: 100%;
+}
 
-  %cursor {
-    cursor: pointer;
-  }
+%cursor {
+  cursor: pointer;
+}
 
-  @mixin set-value($side, $value) {
-    @each $prop in $leftright {
-      #{$side}-#{$prop}: $value;
+@mixin set-value($side, $value) {
+  @each $prop in $leftright {
+    #{$side}-#{$prop}: $value;
+  }
+}
+
+#asideNav {
+  width: auto !important;
+  display: flex;
+  flex-direction: column;
+  border-right: solid 1px #e6e6e6;
+
+  .logo-name {
+    background-color: #03152A !important;
+    @extend %w100;
+    font-weight: 300;
+    z-index: 999;
+
+    p {
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+      font-size: 16px;
+      color: #5e6d82;
     }
   }
 
-  #asideNav {
-    width: auto !important;
-    display: flex;
-    flex-direction: column;
-    border-right: solid 1px #e6e6e6;
-    .logo-name {
-      background-color: #03152A !important;
-      @extend %w100;
-      font-weight: 300;
-      z-index: 999;
-      p {
-        height: 50px;
-        line-height: 50px;
-        text-align: center;
-        font-size: 16px;
-        color: #5e6d82;
-      }
+  .el-menu-vertical:not(.el-menu--collapse) {
+    width: 200px;
+    @extend %h100;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+
+  .el-menu {
+    flex: 1;
+    overflow: inherit;
+    border-right: none;
+
+    &::-webkit-scrollbar {
+      display: none;
     }
-    .el-menu-vertical:not(.el-menu--collapse) {
-      width: 200px;
-      @extend %h100;
-      overflow-y: scroll;
-      overflow-x: hidden;
+
+    .fa {
+      vertical-align: middle;
+      margin-right: 5px;
+      width: 24px;
+      text-align: center;
+      font-size: 18px;
     }
-    .el-menu {
-      flex: 1;
-      overflow: inherit;
-      border-right: none;
-      &::-webkit-scrollbar {
-        display: none;
-      }
-      .fa {
-        vertical-align: middle;
-        margin-right: 5px;
-        width: 24px;
-        text-align: center;
-        font-size: 18px;
-      }
-      .el-menu-item {
-        background-color: #020f1d !important;
-        border-bottom: 1px solid #020f1d;
-        &:hover {
-          color: #ffffff !important;
-          background-color: #375573 !important;
-        }
-      }
-      .el-menu-item.is-active {
-        background-color: #56a9ff !important
-      }
-      .is-opened > .el-submenu__title > .el-icon-arrow-down {
-        color: #ffffff;
-        font-weight: 500;
-        font-size: 18px;
+
+    .el-menu-item {
+      background-color: #020f1d !important;
+      border-bottom: 1px solid #020f1d;
+
+      &:hover {
+        color: #ffffff !important;
+        background-color: #375573 !important;
       }
     }
 
+    .el-menu-item.is-active {
+      background-color: #56a9ff !important
+    }
+
+    .is-opened > .el-submenu__title > .el-icon-arrow-down {
+      color: #ffffff;
+      font-weight: 500;
+      font-size: 18px;
+    }
   }
+
+}
 </style>
