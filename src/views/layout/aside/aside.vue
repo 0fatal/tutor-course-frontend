b785e865cf49ae756b28d18b5a22b01dda25bbc5
ZacharyWin <2816813070@qq.com>
2021-12-14 23:54:13 +0800

    update(aside): new menu for docx module

--- src/views/layout/aside/aside.vue
+++ src/views/layout/aside/aside.vue
@@ -5,7 +5,6 @@
         <p>教学材料生成系统</p>
       </div>
       <el-menu :default-active="$route.path" class="el-menu-vertical"
-               @select="selectMenu"
                :collapse="$store.getters.isCollapse"
                background-color="#03152A"
                text-color="rgba(255,255,255,.7)"
@@ -27,6 +26,15 @@
           <el-menu-item :index="3" route="/course/template/instance">
             实例列表
           </el-menu-item>
+          <el-submenu :index="4">
+            <template slot="title">成绩相关</template>
+            <el-menu-item :index="1" route="/excel/template">
+              模板列表
+            </el-menu-item>
+            <el-menu-item :index="2" route="/excel/template/instance">
+              成绩册
+            </el-menu-item>
+          </el-submenu>
         </el-submenu>
         <el-submenu :index="2">
           <template slot="title">
@@ -54,35 +62,7 @@ export default {
       openTab: ''
     }
   },
-  watch: {
-    // 监听浏览器直接输入路由，将此路由添加到tabnavBox
-    '$route.path': function (val) {
-      this.selectMenu(val)
-    }
-  },
   methods: {
-    selectMenu (key, indexpath) {
-      // 如果不使用 elemenUI 菜单的 vue-router 的模式将用以下方式进行页面跳转 el-menu的router设置为false
-      // this.$router.push(indexpath.join("/"))
-      let router = this.$store.getters.routers
-      let name = ''
-      let navTitle = function (path, routerARR) {
-        for (let i = 0; i < routerARR.length; i++) {
-          if (routerARR[i].children.length > 0 || routerARR[i].path === path) {
-            if (routerARR[i].path === path && routerARR[i].children.length < 1) {
-              name = routerARR[i].name
-              break
-            }
-            navTitle(path, routerARR[i].children)
-          }
-        }
-        return name
-      }
-      this.$store.dispatch('addTab', {
-        title: navTitle(key, router),
-        path: key
-      })
-    }
   }
 }
 </script>
