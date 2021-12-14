d666d46b683c220d1da93d44975ff06ce156e5ad
ZacharyWin <2816813070@qq.com>
2021-12-15 00:09:53 +0800

    update

--- src/views/courseManage/templateInstance.vue
+++ src/views/courseManage/templateInstance.vue
@@ -1,54 +1,50 @@
 <template>
   <div>
     <el-card>
-    <template slot="header">
-      <el-button size="mini" type="warning" @click="() => uploadDialogShow = true">创建新实例</el-button>
-    </template>
-    <el-table border :data="instanceList" style="width: 100%">
-      <el-table-column type="index"></el-table-column>
-      <el-table-column prop="id" width="320" label="实例编号">
-      </el-table-column>
-      <el-table-column prop="templateName" label="模板文件名"></el-table-column>
-      <el-table-column prop="courseName" label="课程名"></el-table-column>
-      <el-table-column prop="updateAt" label="更新日期" width="150">
-        <template slot-scope="scope">
-          {{ dateFormat('YYYY-mm-dd HH:MM', new Date(scope.row.updateAt)) }}
-        </template>
-      </el-table-column>
+      <template slot="header">
+        <h1 v-if="!$route.query.templateId">实例列表</h1>
+        <el-button v-else size="mini" type="warning" @click="handleNew($route.query.templateId)">创建新实例</el-button>
+      </template>
+      <el-table border :data="instanceList" style="width: 100%">
+        <el-table-column type="index"></el-table-column>
+        <el-table-column prop="id" width="320" label="实例编号">
+        </el-table-column>
+        <el-table-column prop="templateName" label="模板文件名"></el-table-column>
+        <el-table-column prop="courseName" label="课程名"></el-table-column>
+        <el-table-column prop="updateAt" label="更新日期" width="150">
+          <template slot-scope="scope">
+            {{ dateFormat('YYYY-mm-dd HH:MM', new Date(scope.row.updateAt)) }}
+          </template>
+        </el-table-column>
 
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            @click="handleInstance(scope.row.filename, scope.row.fid)"
-          >查看
-          </el-button>
-          <el-button
-            size="mini"
-            @click="handleInstance(scope.row.filename, scope.row.fid)"
-          >编辑
-          </el-button>
-          <el-button
-            size="mini"
-            @click="handleInstance(scope.row.filename, scope.row.fid)"
-          >复制
-          </el-button>
-          <el-button
-            size="mini"
-            @click="handleDownload(scope.row.filename, scope.row.fid)"
-          >生成文档
-          </el-button
-          >
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.row.filename, scope.row.fid)"
-          >删除
-          </el-button
-          >
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.row.templateName, scope.row.id)"
+            >编辑
+            </el-button>
+            <el-button
+              size="mini"
+              @click="handleCopy(scope.row.templateName, scope.row.id)"
+            >复制
+            </el-button>
+            <el-button
+              size="mini"
+              @click="handleDownload(scope.row.templateName, scope.row.id)"
+            >生成文档
+            </el-button
+            >
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.row.templateName, scope.row.id)"
+            >删除
+            </el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
   <!-- <div>
@@ -59,7 +55,8 @@
 </template>
 
 <script>
-import {ApiGet} from '../../api/api'
+import {ApiDelete, ApiGet, ApiPost} from '../../api/api'
+import fileDownload from 'js-file-download'
 
 export default {
   name: 'courseInfo',
@@ -69,23 +66,33 @@ export default {
     }
   },
   methods: {
-    async handleDelete (filename, fid) {
+    async handleDelete (templateName, instanceId) {
       try {
-        const confirm = await this.$confirm(`确定删除 [${filename}]？`)
+        const confirm = await this.$confirm(`确定删除实例[${instanceId}]?`)
         if (confirm) {
           try {
-            await this.$request.deleteTemplate(fid)
+            await ApiDelete('/instance/' + instanceId)
             this.$message({
               type: 'success',
-              message: `删除 [${filename}] 成功`
+              message: '删除成功'
             })
 
-            await loadCourses()
+            await this.loadInstance()
           } catch (e) {
             this.$error(e.message)
           }
         }
       } catch (e) {
+      }
+    },
+
+    async handleCopy (templateName, instanceId) {
+      try {
+        await ApiPost('/instance/copy/' + instanceId)
+        this.$message.success('复制成功')
+        await this.loadInstance()
+      } catch (e) {
+        this.$message.error(`复制失败：${e.message}`)
       }
     },
 
@@ -103,8 +110,21 @@ export default {
       this.$forceUpdate()
     },
 
-    handleDownload (filename, fid) {
-      this.$request.getTemplate(fid, filename)
+    async handleNew (templateId) {
+      this.$router.push({
+        name: 'template-instance-edit',
+        query: {
+          templateId
+        }
+      })
+    },
+
+    async handleDownload (templateName, instanceId) {
+      // this.$request.getTemplate(fid, filename)
+      const res =  await ApiGet('/instance/download/' + instanceId,{
+      responseType: 'blob'
+    })
+    fileDownload(res.data,'生成结果.docx')
     },
 
     dateFormat (fmt, date) {
@@ -131,8 +151,13 @@ export default {
       return fmt
     },
 
-    handleInstance (filename, fid) {
-      this.$router.push(`/courseManage/createDocx?fid=${fid}`)
+    handleEdit (templateName, instanceId) {
+      this.$router.push({
+        name: 'template-instance-edit',
+        query: {
+          instanceId
+        }
+      })
     }
   },
   computed: {},
