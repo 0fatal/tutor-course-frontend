88eda5a65dbb80cfb9075016fd8f897246111679
ZacharyWin <2816813070@qq.com>
2021-12-20 21:44:29 +0800

    feat(courseInfo): change column & add `courseState change`

--- src/views/courseManage/courseInfo.vue
+++ src/views/courseManage/courseInfo.vue
@@ -6,18 +6,33 @@
       </template>
       <el-table border :data="courses" style="width: 100%">
         <el-table-column type="index"></el-table-column>
-        <el-table-column sortable prop="courseId" width="320" label="课程编号">
-        </el-table-column>
         <el-table-column prop="courseName" label="课程名"></el-table-column>
         <el-table-column prop="courseNum" label="课程代号"></el-table-column>
+        <el-table-column prop="courseNum" label="学分">
+          <template slot-scope="scope">
+            {{scope.row.credit}} 学分
+          </template>
+        </el-table-column>
+        <el-table-column label="课程时间">
+          <template slot-scope="scope">
+            <span>{{`${scope.row.beginYear}-${scope.row.endYear} 第${scope.row.semester}学期`}}</span>
+          </template>
+        </el-table-column>
 
+        <el-table-column label="开课状态">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.courseState"
+              active-text="开课"
+              :active-value="1"
+              :inactive-value="0"
+              @change="changeCourseState($event,scope.row.courseId)"
+              inactive-text="未开课">
+            </el-switch>
+          </template>
+        </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button
-              size="mini"
-              @click="handleQueryTemplate(scope.row.courseName, scope.row.courseId)"
-            >查看模板
-            </el-button>
             <el-button
               size="mini"
               @click="handleQueryInstance(scope.row.courseName, scope.row.courseId)"
@@ -37,7 +52,7 @@
 </template>
 
 <script>
-import {ApiGet} from '../../api/api'
+import {ApiGet, ApiPatch} from '../../api/api'
 
 export default {
   name: 'courseInfo',
@@ -60,6 +75,14 @@ export default {
         query: {
           courseId: courseId
         }
+      })
+    },
+
+    changeCourseState (courseState, courseId) {
+      console.log('course-state-change', courseState, courseId)
+      ApiPatch('/course/state', {
+        courseId: courseId,
+        courseState: courseState
       })
     },
 
