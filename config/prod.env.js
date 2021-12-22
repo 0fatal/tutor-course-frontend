b6f0a8c6a35723413579b4e8dee6d8ffac39bbbb
ZacharyWin <2816813070@qq.com>
2021-12-22 15:26:01 +0800

    dep

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST:'"http://template-api.zacharywin.top"',
+  API_HOST:'"https://template-api.zacharywin.top"',
   VERSION_CONTROL: false  // yarn build 开启版本控制,开启之后缓存就得每次重新创建,build 的时候得不到优化
 }
